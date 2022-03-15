162ff6a96cff05f112799336dcef9800f286bca0
YousifGad <90724838+YousifGad@users.noreply.github.com>
2022-03-15 19:01:00 +0200

    New Features and bug fixes

--- Advanced TCP-Chat-Server/client.py
+++ Advanced TCP-Chat-Server/client.py
@@ -12,57 +12,62 @@ if nickname == "admin":
 stop = False
 
 def receive():
-    while True:
-        global stop
-        if stop:
-            break
+	while True:
+		global stop
+		if stop:
+			break
 
-        try:
-            message = client.recv(1024).decode("UTF-8")
-            
-            if message == "Nickname:":
-                client.send(nickname.encode("UTF-8"))
+		try:
+			message = client.recv(1024).decode("UTF-8")
+			if message == "Nickname:":
+				client.send(nickname.encode("UTF-8"))
 
-                next_message = client.recv(1024).decode("UTF-8")
-                if next_message == "PASS":
-                    client.send(password.encode("UTF-8"))
-                    if client.recv(1024).decode("UTF-8") == "REFUSE":
-                        print("Wrong password!")
-                        stop = True
+				next_message = client.recv(1024).decode("UTF-8")
+				if next_message == "PASS":
+					client.send(password.encode("UTF-8"))
+					if client.recv(1024).decode("UTF-8") == "REFUSE":
+						print("Wrong password!")
+						stop = True
 
+				elif next_message == "BAN":
+					print("Connection refused because of ban!")
+					stop = True
+				else:
+					print(next_message)
 
-                elif next_message == "BAN":
-                    print("Connection refused because of ban!")
-                    stop = True
-                else:
-                    print(next_message)
+			else:
+				print(message)
 
-            else:
-                print(message)
-
-        except:
-            stop = True
+		except:
+			stop = True
 
 
 def write():
 	while True:
+		global stop
 		if stop:
 			break
-                
+				
 		message = f"{nickname}: {input()}"
-
 		if message[len(nickname)+2:].startswith("/"):
-			if nickname == "admin":
-				if message[len(nickname)+2:].startswith("/kick"):
-					client.send(f"KICK {message[len(nickname)+2+6:]}".encode("UTF-8"))
-
-				elif message[len(nickname)+2:].startswith("/ban"):
-					client.send(f"BAN {message[len(nickname)+2+5:]}".encode("UTF-8"))
-
-			else:
-				if stop == False:
-					print("Commands can be excuted by admin only!")
 			
+			if message[len(nickname)+2:].startswith("/kick"):
+				client.send(f"KICK {message[len(nickname)+2+6:]}".encode("UTF-8"))
+
+			elif message[len(nickname)+2:].startswith("/ban"):
+				client.send(f"BAN {message[len(nickname)+2+5:]}".encode("UTF-8"))
+
+			elif message[len(nickname)+2:].startswith("/clear"):
+				client.send("CLEAR".encode("UTF-8"))
+			
+			else:
+				print("Enter a valid command!")
+			
+		elif message[len(nickname)+2:].lower() == "quit":
+			client.send("QUIT".encode("UTF-8"))
+			client.close()
+			stop = True
+
 		else:
 			if stop == False:
 				client.send(message.encode("UTF-8"))
